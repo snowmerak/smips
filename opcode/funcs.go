066ab751fcb0c3508e613f54d05438036640d067
snowmerak <snowmerak@outlook.com>
2022-04-15 02:22:19 +0900

    add R codes

--- opcode/funcs.go
+++ opcode/funcs.go
@@ -3,6 +3,17 @@ package opcode
 // Funcs are opcode functions.
 const (
 	RAdd = uint8(TypeR + iota)
+	RSub
+	RMul
+	RDiv
+	RRem
+	RAnd
+	ROr
+	RXor
+	RNor
+	RShiftLeft
+	RShiftRightLogical
+	RShiftRightArithmetic
 )
 const (
 	IAdd = uint8(TypeI + iota)
