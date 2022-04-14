ddd8aed218320a1b45708c8c06bf601bd7a694db
snowmerak <snowmerak@outlook.com>
2022-04-14 22:58:28 +0900

    make memory

---
+++ memory/memory.go
@@ -0,0 +1,90 @@
+package memory
+
+import (
+	"runtime"
+	"sort"
+	"sync/atomic"
+)
+
+// pair is a pair of start and end. start <= index < end.
+type pair struct {
+	start uint64
+	end   uint64
+}
+
+// Memory is a memory. lol.
+type Memory struct {
+	space []uint64
+	marks []pair
+	lock  int64
+}
+
+// New returns a new Memory.
+func New(size int) *Memory {
+	return &Memory{
+		space: make([]uint64, size*8),
+		marks: make([]pair, 0),
+	}
+}
+
+// Lock locks the memory.
+func (m *Memory) Lock() {
+	for atomic.CompareAndSwapInt64(&m.lock, 0, 1) {
+		runtime.Gosched()
+	}
+}
+
+// Unlock unlocks the memory.
+func (m *Memory) Unlock() {
+	atomic.StoreInt64(&m.lock, 0)
+}
+
+// GetFreeSpace returns the first free space.
+func (m *Memory) GetFreeSpace(size uint64) uint64 {
+	prev := uint64(0)
+	for _, v := range m.marks {
+		if v.start == 0 {
+			continue
+		}
+		if v.start-prev >= size {
+			return prev
+		}
+		prev = v.end
+	}
+
+	alloced := uint64(len(m.space)) - prev
+	m.space = append(m.space, make([]uint64, size-alloced+1)...)
+	return prev
+}
+
+// Alloc allocates a new space.
+func (m *Memory) Alloc(size uint64) {
+	m.Lock()
+	defer m.Unlock()
+	if size == 0 {
+		return
+	}
+
+	address := m.GetFreeSpace(size)
+	m.marks = append(m.marks, pair{address, address + size})
+	return
+}
+
+// Free frees a space.
+func (m *Memory) Free(address uint64) {
+	m.Lock()
+	defer m.Unlock()
+	idx := sort.Search(len(m.marks), func(i int) bool {
+		return m.marks[i].start >= address
+	})
+	if idx == len(m.marks) || m.marks[idx].start != address {
+		return
+	}
+	s := m.marks[idx].start
+	e := m.marks[idx].end
+	for s < e {
+		m.space[s] = 0
+		s++
+	}
+	m.marks = append(m.marks[:idx], m.marks[idx+1:]...)
+}
