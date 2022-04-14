241b33d7c6ffd3b5ecb973d3df0df3e637ad2dd2
snowmerak <snowmerak@outlook.com>
2022-04-14 23:38:09 +0900

    combine stack to process

--- process/process.go
+++ process/process.go
@@ -2,10 +2,12 @@ package process
 
 import (
 	"github.com/snowmerak/smips/memory"
-	"github.com/snowmerak/smips/stack"
+	"github.com/snowmerak/smips/opcode"
+	"github.com/snowmerak/smips/register"
 )
 
 type Process struct {
-	memory *memory.Memory
-	stack  *stack.Stack
+	memory        *memory.Memory
+	opcodes       []opcode.OpCode
+	registerStack []*register.Register
 }
