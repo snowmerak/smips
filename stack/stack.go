b4044f3257f1fa439cb75f0a59f08b70e71fa2bf
snowmerak <snowmerak@outlook.com>
2022-04-14 23:31:06 +0900

    add global memory pointer

--- stack/stack.go
+++ stack/stack.go
@@ -1,11 +1,13 @@
 package stack
 
 import (
+	"github.com/snowmerak/smips/memory"
 	"github.com/snowmerak/smips/opcode"
 	"github.com/snowmerak/smips/register"
 )
 
 type Stack struct {
-	opcodes []opcode.OpCode
-	reg     register.Register
+	globalMemory *memory.Memory
+	opcodes      []opcode.OpCode
+	reg          register.Register
 }
