b624e1175193f0d2baf8764d5952ca865630240f
snowmerak <snowmerak@outlook.com>
2022-04-17 19:32:25 +0900

    create opcode

--- opcode/opcode.go
+++ opcode/opcode.go
@@ -1,46 +1,36 @@
 package opcode
 
-// OpCode
-type OpCode uint64
+// Opcode is a single instruction in the VM.
+// 8 bit data, 16 bit ra, 16 bit rb, 16 bit rd, 8 bit op.
+// 40 bit wide-data, 16 bit rd, 8 bit op.
+type Opcode uint64
 
-// Function is the function field.
-func (op OpCode) Function() uint8 {
-	return uint8(op & 0b11111111)
+// Data is the 8 bit data field of the opcode.
+func (o Opcode) Data() uint8 {
+	return uint8(o >> 56)
 }
 
-//Type
-type Type uint8
-
-// OpCodeTypes
-const (
-	TypeR = Type(0b00000000)
-	TypeI = Type(0b10000010)
-	TypeJ = Type(0b11110000)
-)
-
-// Type returns the type of the opcode.
-func (op OpCode) Type() Type {
-	t := Type(op & 0b11111111)
-	if t >= TypeJ {
-		return TypeJ
-	}
-	if t < TypeJ && t >= TypeI {
-		return TypeI
-	}
-	return TypeR
+// Ra is the 16 bit ra field of the opcode.
+func (o Opcode) Ra() uint16 {
+	return uint16(o >> 48)
 }
 
-// NewR creates a new R-type opcode.
-func NewR(rsA, rsB, rd uint8, data uint32, funt uint8) R {
-	return R(uint64(rsA)<<56 | uint64(rsB)<<48 | uint64(rd)<<40 | uint64(data)<<8 | uint64(funt))
+// Rb is the 16 bit rb field of the opcode.
+func (o Opcode) Rb() uint16 {
+	return uint16(o >> 32)
 }
 
-// NewI creates a new I-type opcode.
-func NewI(rs, rd uint8, imm uint32, funt uint8) I {
-	return I(uint64(rs)<<56 | uint64(rd)<<48 | uint64(imm)<<8 | uint64(funt))
+// WideData is the 40 bit wide-data field of the opcode.
+func (o Opcode) WideData() uint64 {
+	return uint64(o >> 14)
 }
 
-// NewJ creates a new J-type opcode.
-func NewJ(address uint64, funt uint8) J {
-	return J(uint64(address)<<8 | uint64(funt))
+// Rd is the 16 bit rd field of the opcode.
+func (o Opcode) Rd() uint16 {
+	return uint16(o >> 16)
+}
+
+// Op is the 8 bit op field of the opcode.
+func (o Opcode) Op() uint8 {
+	return uint8(o >> 8)
 }
