d7596190c23ea120ebf261519d135bb700250fb1
snowmerak <snowmerak@outlook.com>
2022-04-18 07:11:24 +0900

    create syscall

---
+++ process/systemcall.go
@@ -0,0 +1,19 @@
+package process
+
+import "github.com/snowmerak/smips/opcode"
+
+func SystemCall(p *Process, op opcode.Opcode) {
+	switch opcode.Op(op.Op()) {
+	case opcode.OpenFile:
+	case opcode.CloseFile:
+		p.stack.Set(op.Rd(), p.files.CloseFile(p.stack.Get(op.Ra())))
+	case opcode.ReadFile:
+		p.stack.Set(op.Rd(), p.files.ReadFile(p.stack.Get(op.Ra()), op.Data()))
+	case opcode.ReadFileAt:
+		p.stack.Set(op.Rd(), p.files.ReadFileAt(p.stack.Get(op.Ra()), p.stack.Get(op.Rb()), op.Data()))
+	case opcode.WriteFile:
+		p.stack.SetHi(p.files.WriteFile(p.stack.Get(op.Ra()), op.Data(), p.stack.Get(op.Rd())))
+	case opcode.WriteFileAt:
+		p.stack.SetHi(p.files.WriteFileAt(p.stack.Get(op.Ra()), p.stack.Get(op.Rb()), op.Data(), p.stack.Get(op.Rd())))
+	}
+}
