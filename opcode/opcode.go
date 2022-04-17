4817e830fbfa9d24f30b22865538e48ae215501e
snowmerak <snowmerak@outlook.com>
2022-04-17 19:37:01 +0900

    make constructor

--- opcode/opcode.go
+++ opcode/opcode.go
@@ -5,6 +5,14 @@ package opcode
 // 40 bit wide-data, 16 bit rd, 8 bit op.
 type Opcode uint64
 
+func NewRegularOpCode(data uint8, ra, rb, rd uint16, op uint8) Opcode {
+	return Opcode((uint64(data) << 56) | (uint64(ra) << 40) | (uint64(rb) << 24) | (uint64(rd) << 8) | uint64(op))
+}
+
+func NewWideOpCode(data uint64, rd uint16, op uint8) Opcode {
+	return Opcode((uint64(data) << 24) | (uint64(rd) << 8) | uint64(op))
+}
+
 // Data is the 8 bit data field of the opcode.
 func (o Opcode) Data() uint8 {
 	return uint8(o >> 56)
