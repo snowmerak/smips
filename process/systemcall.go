fe4ff47bada710282dc5c08869a8a2920aedf30f
snowmerak <snowmerak@outlook.com>
2022-04-18 20:41:21 +0900

    impl openfile

--- process/systemcall.go
+++ process/systemcall.go
@@ -5,6 +5,9 @@ import "github.com/snowmerak/smips/opcode"
 func SystemCall(p *Process, op opcode.Opcode) {
 	switch opcode.Op(op.Op()) {
 	case opcode.OpenFile:
+		path := ReadString(&p.memory, p.stack.Get(op.Ra()), 0)
+		fd := p.files.OpenFile(string(path), uint8(p.stack.Get(op.Rb())))
+		p.stack.Set(op.Rd(), uint64(fd))
 	case opcode.CloseFile:
 		p.stack.Set(op.Rd(), p.files.CloseFile(p.stack.Get(op.Ra())))
 	case opcode.ReadFile:
